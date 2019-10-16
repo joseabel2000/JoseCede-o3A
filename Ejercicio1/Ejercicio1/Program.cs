23ad03c5acbf75908e20c59a30335c0ff622207b
josea <josea@LAPTOP-F55GO9HU>
2019-10-16 14:15:29 -0500

    ouhyo

--- Ejercicio1/Ejercicio1/Program.cs
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -10,7 +10,25 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-            Console.Write("asfakdfs");
+            Console.WriteLine("Ingrese el tamaño de la serie de números:  ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] Array = new int[n];
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Console.WriteLine("Ingrese dato " + (i + 1) + ":  ");
+                Array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int mayor;
+            mayor = Array[0];
+            for (int j = 0; j < Array.Length; j++)
+            {
+                if (Array[j] > mayor)
+                {
+                    mayor = Array[j];
+                }
+            }
+            Console.WriteLine("El número mayor es:   " + mayor);
+            Console.ReadKey();
         }
     }
 }
