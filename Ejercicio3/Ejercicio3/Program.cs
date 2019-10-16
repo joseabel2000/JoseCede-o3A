d8d0975693dc37c5fc8d2deb95740ebb85f3ad73
josea <josea@LAPTOP-F55GO9HU>
2019-10-16 14:26:45 -0500

    jwnjcuew

---
+++ Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cantidad, precio, total;
+            Console.WriteLine("Ingrese la cantidad:  ");
+            cantidad = Convert.ToDouble(Console.ReadLine());
+            if (cantidad != 0)
+            {
+                Console.WriteLine("Ingrese el precio:  ");
+                precio = Convert.ToDouble(Console.ReadLine());
+                total = Convert.ToDouble(cantidad * precio);
+                Console.WriteLine("El total a pagar:  " + total);
+            }
+            else
+            {
+                Console.WriteLine("Gracias por su visita");
+            }
+            Console.ReadKey();
+        }
+    }
+}
