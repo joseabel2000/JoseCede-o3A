8d28897cdfa6fbc7d007714380f828f820078df5
josea <josea@LAPTOP-F55GO9HU>
2019-10-16 14:47:31 -0500

    ejercicio7

---
+++ Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0, d = 101;
+            do
+            {
+                c++;
+                d--;
+                Console.WriteLine(" {0} {1} ", c, d);
+            } while (c < 101 && d > 1);
+            Console.ReadKey();
+        }
+    }
+}
