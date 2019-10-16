898f9cc1809789a8017bc9b08a3a1e7f10e16aad
josea <josea@LAPTOP-F55GO9HU>
2019-10-16 14:23:07 -0500

    sadscdsfef

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un número para generar el cuadrado de asteriscos:  ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            //Linea superior
+            for (int i = 0; i < numero; i++)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine();
+
+            //centro del cuadrado
+            for (int i = 0; i < numero - 2; i++)
+            {
+                Console.Write("* ");
+                for (int j = 0; j < numero - 2; j++)
+                {
+                    Console.Write("  ");
+                }
+                Console.WriteLine("* ");
+            }
+            //linea inferior 
+            for (int i = 0; i < numero; i++)
+            {
+                Console.Write("* ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
